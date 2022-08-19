47e0def007397a90554829af352ff34ef3dcad80
DevPetyr <devpetyr911@gmail.com>
2022-08-19 19:39:12 +0500

    first commit
    iyzico payment laravel

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+    public function pay()
+    {
+        $request = new \Iyzipay\Request\CreateCheckoutFormInitializeRequest();
+        $request->setLocale(\Iyzipay\Model\Locale::TR);
+        $request->setConversationId("987654321");
+        $request->setPrice("1");
+        $request->setPaidPrice("1.2");
+        $request->setCurrency(\Iyzipay\Model\Currency::TL);
+        $request->setBasketId("B67832");
+        $request->setPaymentGroup(\Iyzipay\Model\PaymentGroup::PRODUCT);
+        $request->setCallbackUrl("http://127.0.0.1:8000/get-data");
+        $request->setEnabledInstallments(array(2, 3, 6, 9));
+
+        $buyer = new \Iyzipay\Model\Buyer();
+        $buyer->setId("BY789");
+        $buyer->setName("Dev");
+        $buyer->setSurname("Devpetyr");
+        $buyer->setGsmNumber("+923408736583");
+        $buyer->setEmail("devpetyr911@gmail.com");
+        $buyer->setIdentityNumber("74300864791");
+        $buyer->setLastLoginDate("2015-10-05 12:43:35");
+        $buyer->setRegistrationDate("2013-04-21 15:12:09");
+        $buyer->setRegistrationAddress("Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1");
+        $buyer->setIp("85.34.78.112");
+        $buyer->setCity("Istanbul");
+        $buyer->setCountry("Turkey");
+        $buyer->setZipCode("34732");
+
+        $request->setBuyer($buyer);
+        $shippingAddress = new \Iyzipay\Model\Address();
+        $shippingAddress->setContactName("Jane Doe");
+        $shippingAddress->setCity("Istanbul");
+        $shippingAddress->setCountry("Turkey");
+        $shippingAddress->setAddress("Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1");
+        $shippingAddress->setZipCode("34742");
+        $request->setShippingAddress($shippingAddress);
+
+        $billingAddress = new \Iyzipay\Model\Address();
+        $billingAddress->setContactName("Jane Doe");
+        $billingAddress->setCity("Istanbul");
+        $billingAddress->setCountry("Turkey");
+        $billingAddress->setAddress("Nidakule Göztepe, Merdivenköy Mah. Bora Sok. No:1");
+        $billingAddress->setZipCode("34742");
+        $request->setBillingAddress($billingAddress);
+
+        $basketItems = array();
+        $firstBasketItem = new \Iyzipay\Model\BasketItem();
+        $firstBasketItem->setId("BI101");
+        $firstBasketItem->setName("asd");
+        $firstBasketItem->setCategory1("asd");
+        $firstBasketItem->setCategory2("asd");
+        $firstBasketItem->setItemType(\Iyzipay\Model\BasketItemType::PHYSICAL);
+        $firstBasketItem->setPrice("0.3");
+        $basketItems[0] = $firstBasketItem;
+
+        $secondBasketItem = new \Iyzipay\Model\BasketItem();
+        $secondBasketItem->setId("BI102");
+        $secondBasketItem->setName("Game code");
+        $secondBasketItem->setCategory1("Game");
+        $secondBasketItem->setCategory2("Online Game Items");
+        $secondBasketItem->setItemType(\Iyzipay\Model\BasketItemType::VIRTUAL);
+        $secondBasketItem->setPrice("0.5");
+
+        $basketItems[1] = $secondBasketItem;
+        $thirdBasketItem = new \Iyzipay\Model\BasketItem();
+        $thirdBasketItem->setId("BI103");
+        $thirdBasketItem->setName("Usb");
+        $thirdBasketItem->setCategory1("Electronics");
+        $thirdBasketItem->setCategory2("Usb / Cable");
+        $thirdBasketItem->setItemType(\Iyzipay\Model\BasketItemType::PHYSICAL);
+        $thirdBasketItem->setPrice("0.2");
+        $basketItems[2] = $thirdBasketItem;
+        $request->setBasketItems($basketItems);
+
+        $checkoutFormInitialize = \Iyzipay\Model\CheckoutFormInitialize::create($request, $this->config());
+
+        print_r($checkoutFormInitialize);
+//        return response()->json($checkoutFormInitialize);
+
+
+    }
+
+
+    public function getdata()
+    {
+        $request = new \Iyzipay\Request\RetrieveCheckoutFormRequest();
+        $request->setLocale(\Iyzipay\Model\Locale::TR);
+        $request->setConversationId("987654321");
+        $request->setToken("b3d82317-b5a3-4bff-aa89-78896304a3c7");
+        $checkoutForm = \Iyzipay\Model\CheckoutForm::retrieve($request, $this->config());
+        print_r($checkoutForm);
+//        return $checkoutForm;
+    }
+    public function config()
+    {
+        $options = new \Iyzipay\Options();
+        $options->setApiKey('sandbox-sG7Z92yHE6LZIPgCFbRVknGX0x0nOi8o');
+        $options->setSecretKey('sandbox-QdUHFD8fe39fp5ZpY0wLsmQA0yTmcFqN');
+        $options->setBaseUrl('https://sandbox-api.iyzipay.com');
+
+        return $options;
+    }
+}
